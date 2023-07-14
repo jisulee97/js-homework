844733e88f74c3ec76cdd552b1e2c7afb045e060
leejisu <ejeesoo97@gmail.com>
2023-07-14 22:42:03 +0900

    자바스크립트 로그인 페이지 구현하기 초안 커밋

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,39 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+const inputId = document.querySelector("#userEmail");
+const passWord = document.querySelector("#userPassword");
+const loginButton = document.querySelector(".btn-login");
 
+inputId.addEventListener("input", () => {
+  let idValue = emailReg(inputId.value);
+  if (idValue) {
+    inputId.classList.remove("is--invalid");
+  } else inputId.classList.add("is--invalid");
+});
 
+passWord.addEventListener("input", () => {
+  let passwordValue = pwReg(passWord.value);
+  if (passwordValue) {
+    passWord.classList.remove("is--invalid");
+  } else passWord.classList.add("is--invalid");
+});
 
-
-
-
-
-
-
-
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault(); // 버튼의 submit 이벤트가 작동되는것을 막기 위한 코드
+  if (inputId.value === user.id && passWord.value === user.pw) {
+    window.location.href = "welcome.html";
+  } else alert("아이디와 비밀번호가 일치하지 않습니다.");
+});
